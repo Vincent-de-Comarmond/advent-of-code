82504f3620f7bda22113e0d0ac3a7b05c5a399de
Vincent De Comarmond <vincentdc@synthesis.co.za>
2025-11-09 20:09:18 +0200

    Renamed garbage and made updates

--- 2015/19/b.c
+++ 2015/19/b.c
@@ -1,233 +1,154 @@
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define EL_SIZE 3
+#define SRC_SIZE 3
+#define _NT 45
 #define WIDTH 550
-#define TRANSFORMS 50
-#define MSIZE 10
-#define MAX (int)1e6
+#define MAX_DEST 10
 
-typedef char Element[EL_SIZE];
-typedef char Molecule[WIDTH][EL_SIZE];
+int NUM_TRANS = 0;
+char TARGET[WIDTH] = {[0 ... WIDTH - 1] = '\0'};
+char SOURCES[_NT][3] = {[0 ... _NT - 1] = {'\0', '\0', '\0'}};
+char DESTINATIONS[_NT][MAX_DEST + 1] = {
+    [0 ... _NT - 1] = {[0 ... MAX_DEST] = '\0'}};
 
-static char dests[TRANSFORMS][MSIZE][EL_SIZE] = {
-    [0 ... TRANSFORMS - 1] = {[0 ... MSIZE - 1] = {'\0', '\0', '\0'}}};
-static int dest_sizes[TRANSFORMS] = {[0 ... TRANSFORMS - 1] = 0};
-
-static char sources[TRANSFORMS][EL_SIZE] = {
-    [0 ... TRANSFORMS - 1] = {'\0', '\0', '\0'}};
-
-static char target[WIDTH][EL_SIZE] = {[0 ... WIDTH - 1] = {'\0', '\0', '\0'}};
-
-int read_element(char *mol_str, Element element) {
-  element[0] = mol_str[0];
-  if (strlen(mol_str) == 1)
-    return 1;
-
-  if ('a' <= mol_str[1] && mol_str[1] <= 'z') {
-    element[1] = mol_str[1];
-    return 2;
-  }
-  return 1;
-}
-
-int read_molecule(char *mol_str, Molecule molecule, int number) {
-  int accum = 0, idx = 0, offset = 0;
-  char copy[WIDTH] = {[0 ... WIDTH - 1] = '\0'};
-  strcpy(copy, mol_str);
-
-  if (number == -1)
-    while (0 < strlen(copy)) {
-      offset = read_element(copy, molecule[idx]);
-      accum += offset;
-      strncpy(copy, mol_str + accum, EL_SIZE);
-      idx++;
-    }
-  else {
-    while (idx < number && 0 < strlen(copy)) {
-      offset = read_element(copy, molecule[idx]);
-      accum += offset;
-      strncpy(copy, mol_str + accum, EL_SIZE);
-      idx++;
-    }
-  }
-  return idx;
-}
-
-typedef struct _NBNUMS {
-  int max_expand;
-  int num_poss_react;
-  int target_size;
-} NBNUMS;
-
-NBNUMS define_problem(char *filename) {
+void parse_input(char *filename) {
   FILE *file = fopen(filename, "r");
-  if (file == NULL)
+  if (file == NULL) {
+    printf("FAILED - could not open file");
     exit(EXIT_FAILURE);
+  }
 
-  int idx = 0, max_expansion = 0, targ_size = 0, tmp = 0;
-  bool is_targ = false;
-  char buffer[WIDTH];
-  char *destination;
+  bool istarget = false;
+  char buffer[WIDTH] = {[0 ... WIDTH - 1] = '\0'};
 
   while (fgets(buffer, WIDTH, file)) {
     buffer[strcspn(buffer, "\n")] = 0;
+
     if (strlen(buffer) == 0) {
-      is_targ = true;
+      istarget = true;
       continue;
     }
 
-    if (is_targ) {
-      targ_size = read_molecule(buffer, target, -1);
-      printf("Target molecules: %d\n", targ_size);
+    if (istarget) {
+      memcpy(TARGET, buffer, WIDTH);
       break;
-    } else {
-      strcpy(sources[idx], strtok(buffer, " => "));
-      destination = strtok(NULL, " => ");
-      tmp = read_molecule(destination, dests[idx], -1);
-      max_expansion = max_expansion < tmp ? tmp : max_expansion;
-      dest_sizes[idx] = tmp;
-      idx++;
     }
+
+    // Reverse source and destination from part 1
+    char *source = strtok(buffer, " => ");
+    char *dest = strtok(NULL, " => ");
+    memcpy(SOURCES[NUM_TRANS], source, SRC_SIZE * sizeof(char));
+    memcpy(DESTINATIONS[NUM_TRANS], dest, strlen(dest) * sizeof(char));
+    NUM_TRANS++;
   }
   fclose(file);
-
-  NBNUMS nbnums;
-  nbnums.max_expand = max_expansion;
-  nbnums.num_poss_react = idx;
-  nbnums.target_size = targ_size;
-  return nbnums;
 }
 
-bool can_reduce(int targ_size, char this_target[targ_size][EL_SIZE],
-                int reduction_size,
-                char this_reduction[reduction_size][EL_SIZE], int targ_offset,
-                int stop_idx) {
+int limited_search_replace(char target[WIDTH], char *search, char *replace,
+                           int start, int limit) {
+  int targlen = strlen(target);
+  int searchlen = strlen(search);
+  int repllen = strlen(replace);
 
-  if (stop_idx < targ_offset + reduction_size)
-    return false;
-  for (int i = 0; i < reduction_size; i++)
-    if (strcmp(this_target[targ_offset + i], this_reduction[i]) != 0)
-      return false;
-  return true;
+  char copy[WIDTH];
+  strcpy(copy, target);
+  int end = limit < targlen ? limit - searchlen : targlen - searchlen;
+
+  int i = 0;
+  for (i = start; i < end; i++) {
+    bool match = true;
+    for (int j = 0; j < searchlen; j++) {
+      if (search[j] != target[i + j]) {
+        match = false;
+        break;
+      }
+    }
+
+    if (!match)
+      continue;
+
+    for (int j = 0; j < repllen; j++)
+      target[i + j] = replace[j];
+
+    for (int j = i + repllen; j < targlen + repllen - searchlen; j++)
+      target[j] = target[j + repllen - searchlen];
+
+    printf("%s\n", copy);
+    printf("%s\n", target);
+  }
+
+  // IDEA is stupid-simple ... if returned index is limit nothing has been
+  // replaced AND nothing can be replace. If index is less than final we have to
+  // try all replacements on the next character from the index
+  return i + searchlen;
 }
 
 void solve(char *filename) {
-  NBNUMS nbnums = define_problem(filename);
+  parse_input(filename);
 
-  /* printf("Max size: %d\nNum transforms: %d\nTarget size: %d\n", */
-  /*        nbnums.max_expand, nbnums.num_poss_react, nbnums.target_size); */
+  int idx = 0, idx_cpy = 0, gen = 0;
+  char tmp[WIDTH];
+  char(*targets)[WIDTH] = malloc(sizeof(char[(int)1e3][WIDTH]));
+  memset(targets, '\0', sizeof(char[(int)1e3][WIDTH]));
 
-  for (int i = 0; i < nbnums.num_poss_react; i++) {
-    printf("Source: %s\n", sources[i]);
-    for (int j = 0; j < dest_sizes[i]; j++)
-      printf("\t-> %s\n", dests[i][j]);
-  }
+  char(*targcpy)[WIDTH] = malloc(sizeof(char[(int)1e3][WIDTH]));
+  memset(targcpy, '\0', sizeof(char[(int)1e3][WIDTH]));
 
-  /* for (int i = 0; i < nbnums.target_size; i++) */
-  /*   printf(i == 0 ? "%s" : " %s", target[i]); */
-  /* printf("\n"); */
+  strcpy(targets[idx], TARGET);
+  idx++;
 
-  int gens = 0, max_gen = 1e4, targ_idx = 0, targ_cpy_idx = 0;
-  size_t size = MAX * nbnums.target_size * EL_SIZE * sizeof(char);
+  while (gen < (int)1e6) {
+    for (int i = 0; i < idx; i++) {
+      for (int j = 0; j < NUM_TRANS; j++) {
+        char *source = SOURCES[j];
+        char *dest = DESTINATIONS[j];
+        int start_idx = 0;
 
-  int targ_sizes[MAX] = {[0 ... MAX - 1] = 0};
-  int targ_size_copy[MAX] = {[0 ... MAX - 1] = 0};
-  targ_sizes[0] = nbnums.target_size;
+        while (start_idx < MAX_DEST) {
+          memset(tmp, '\0', sizeof(tmp));
+          strcpy(tmp, targets[i]);
 
-  char(*targs)[nbnums.target_size][EL_SIZE];
-  char(*targs_cpy)[nbnums.target_size][EL_SIZE];
+          start_idx = limited_search_replace(tmp, dest, source, start_idx + 1,
+                                             MAX_DEST);
 
-  targs = malloc(MAX * sizeof(*targs));
-  targs_cpy = malloc(MAX * sizeof(*targs_cpy));
-  char tmp[nbnums.target_size][EL_SIZE];
+          if (-1 < start_idx && start_idx < MAX_DEST) {
+            printf("Start idx: %d\n", start_idx);
+            printf("Tried to replace %s -> %s\n", dest, source);
+            printf("Input: %s\n", targets[i]);
+            printf("Output: %s\n", tmp);
 
-  memset(targs, '\0', size);
-  memset(targs_cpy, '\0', size);
-  memset(tmp, '\0', nbnums.target_size * EL_SIZE * sizeof(char));
-
-  for (int i = 0; i < nbnums.target_size; i++)
-    strcpy(targs[targ_idx][i], target[i]);
-  targ_idx++;
-
-  while (gens < max_gen) {
-    for (int i = 0; i < targ_idx; i++) {
-      /* for (int j = 0; j < targ_sizes[i]; j++) */
-      /*   printf("%s ", targs[i][j]); */
-      printf("\nGeneration %d/%d\nNum candidates: %d\n", i, max_gen, targ_idx);
-      printf("\nsize %d\n", targ_sizes[i]);
-
-      for (int j = 0; j < nbnums.max_expand; j++) {
-        for (int k = 0; k < nbnums.num_poss_react; k++) {
-          if (can_reduce(nbnums.target_size, targs[i], dest_sizes[k], dests[k],
-                         j, nbnums.max_expand)) {
-
-            /* printf("Replacing: "); */
-            /* for (int a = 0; a < dest_sizes[k]; a++) */
-            /*   printf("%s ", dests[k][a]); */
-            /* printf("\nWith: %s\n", sources[k]); */
-
-            int new_size = targ_sizes[i] + 1 - dest_sizes[k];
-            strcpy(tmp[0], sources[k]);
-            if (new_size < 10) {
-              printf("Conversion: ");
-              for (int l = 0; l < dest_sizes[k]; l++) {
-                printf(" %s", dests[k][l]);
-              }
-              printf("\n -> %s\n", sources[k]);
-            }
-
-            if ((new_size == 1) && (tmp[0][0] == 'e')) {
-              printf("Number generations: %d\n", gens);
-              return;
-            }
-
-            /* printf("Here with new size %d\n", new_size); */
-            for (int l = 1; l < new_size; l++)
-              strcpy(tmp[l], targs[i][dest_sizes[k] + l - 1]);
-
-            bool is_dup = false;
-            for (int l = 0; l < targ_cpy_idx; l++) {
-              is_dup = true;
-              for (int m = 0; m < new_size; m++)
-                if (strcmp(targs_cpy[l][m], tmp[m]) != 0) {
-                  is_dup = false;
-                  break;
-                }
-              if (is_dup)
-                break;
-            }
-
-            if (!is_dup) {
-              memcpy(targs_cpy[targ_cpy_idx], tmp, sizeof(tmp));
-              targ_size_copy[targ_cpy_idx] = new_size;
-              if (new_size < 10) {
-                printf("Candidate %d\n", i);
-                for (int l = 0; l < new_size; l++)
-                  printf("%s ", targs_cpy[targ_cpy_idx][l]);
-                printf("\n");
-              }
-
-              targ_cpy_idx++;
-            }
-
-            memset(tmp, '\0', sizeof(tmp));
+            strcpy(targcpy[idx_cpy], tmp);
+            idx_cpy++;
           }
         }
       }
     }
-    memcpy(targs, targs_cpy, size);
-    memcpy(targ_sizes, targ_size_copy, MAX * sizeof(int));
-    targ_idx = targ_cpy_idx;
 
-    targ_cpy_idx = 0;
-    memset(targ_size_copy, 0, MAX * sizeof(int));
-    memset(targs_cpy, '\0', size);
-    gens++;
+    idx = 0;
+    memset(targets, '\0', sizeof(char[(int)1e3][WIDTH]));
+
+    for (int i = 0; i < idx_cpy; i++) {
+      bool duplicate = false;
+
+      for (int j = 0; j < idx; j++) {
+        if (strcmp(targets[j], targcpy[i]) == 0) {
+          duplicate = true;
+          break;
+        }
+      }
+
+      if (!duplicate) {
+        strcpy(targets[idx], targcpy[i]);
+        idx++;
+      }
+    }
+
+    idx_cpy = 0;
+    memset(targcpy, '\0', sizeof(char[(int)1e3][WIDTH]));
   }
 }
 
@@ -239,5 +160,4 @@ int main(int argc, char *argv[argc]) {
   end = clock();
 
   printf("Execution time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-  return EXIT_SUCCESS;
 }
